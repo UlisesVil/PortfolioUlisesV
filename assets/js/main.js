1d6ecccc306d93c3e0d1a31ed052e5f3967c1e9d
github_UlisesVil <ulisesvil5@hotmail.com>
2021-03-05 14:59:38 -0600

    Auto-generated commit

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function() {
+
+    //Nav-Bar
+    $(".mat-drawer-backdrop").click(function(){
+        $('#transparentWall').removeAttr('style','background:red; width: 100%; height: 100vh; ');
+        $('mat-toolbar').removeAttr('style','background:red;');
+        console.log('checa otra cosa');
+    });
+  /*  $(".linkNav").click(function(){
+        $('#transparentWall').removeAttr('style','background:black; width: 100%; height: 100vh; ');
+        $('mat-toolbar').removeAttr('style','background:black; display:none');
+       
+    });
+    $("#buttonNav2").click(function(){
+        $('#transparentWall').removeAttr('style','background:black; width: 100%; height: 100vh; ');
+        $('mat-toolbar').removeAttr('style','background:black; display:none');
+    });
+*/
+/*
+    $('.element').mouseover(function(){
+        $(this).find('h2').css('display','block');
+
+    });
+    
+    $('.element').mouseleave(function(){
+        $(this).find('h2').css('display','none');
+    });*/
+
+
+
+/*
+$(window).ready(function(){
+
+    var width = $(window).width();
+    console.log(width);
+    if(width<'696'){
+        $('.skillName').css('width','110px');
+     }
+    /*let sizeElement=$('.element').css('width');
+        let size=parseInt(sizeElement.slice(0,(sizeElement.length-2)));
+        console.log(size);
+        if(size<'111'){
+           $('.skillName').css('width','110px');
+        }
+
+    });*/
+
+
+    //header
+
+    
+/*
+//header
+$(document.body).on('click', 'a[href*="#"]', function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 900);
+  });
+  
+  $(window).on('scroll', function() {
+    var scrolled = $(window).scrollTop();
+    var height = $(window).height();
+    if (scrolled < height) {
+      $('.full-image').css({
+        'top': -scrolled * .5
+      });
+    }
+  });
+*/
+
+
+
+});  
