74975968ca9d948e2980540ebd590d65d45f05c6
github_UlisesVil <ulisesvil5@hotmail.com>
2021-05-06 23:48:58 -0500

    Resume Download function added

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../../services/message.service';
+//import { ViewDataService } from '../../services/view-data.service';
+import { DownloadService } from '../../services/download.service';
+import * as fileSaver from 'file-saver';
 import swal from 'sweetalert';
+import { onErrorResumeNext } from 'rxjs';
 
 @Component({
   selector: 'contact-form',
@@ -8,11 +12,16 @@ import swal from 'sweetalert';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+  //menuItem = null;
+  fileName= 'UlisesVillaResume.pdf';
 
-  constructor(public _MessageService: MessageService) {
-  }
+  constructor(
+    public _MessageService: MessageService,
+    private downloadService: DownloadService
+    ) { }
 
   ngOnInit(): void {
+    //this.menuItem = this.viewDataService.getMenuList()[3];
   }
 
   contactForm(form){
@@ -20,5 +29,32 @@ export class ContactFormComponent implements OnInit {
       swal("Thank You!!!", "I have received your message and I will contact you shortly.",'success');
     });
   }
+
+
+
+  returnBlob(res): Blob{
+    console.log('File Downloaded');
+    return new Blob([res], {type: 'application/pdf'});
+  }
+
+  download(){
+    this.downloadService.downloadFile(this.fileName).subscribe( res=> {
+      if (res) {
+        const url = window.URL.createObjectURL(this.returnBlob(res));
+        window.open(url);
+      }
+    });
+  }
+
+  download2(){
+    this.downloadService.downloadFile(this.fileName).subscribe( res=> {
+      if (res) {
+        fileSaver.saveAs(this.returnBlob(res), this.fileName);
+      }
+    });
+  }
+
+
+
 
 }
